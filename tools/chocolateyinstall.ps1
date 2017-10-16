e6630d219b738aefcd3129e0f67d37cbd4f42a21
Vladislav Nemichev <vlpro@me.com>
2017-10-16 11:14:08 +0500

    change log path

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = 'MS MPI'
-  fileType      = 'exe'
-  file         = Join-Path $toolsDir 'MSMpiSetup.exe'
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'exe'
+    file           = Join-Path $toolsDir 'MSMpiSetup.exe'
 
-  softwareName  = 'Microsoft MPI (8.1.12438.1084)'
+    softwareName   = 'Microsoft MPI (8.1.12438.1084)'
 
-  silentArgs    = "-unattend -force -full -log `"$($env:TEMP)\$($packageName).MSMpi.MsiInstall.log`""
-  validExitCodes= @(0)
+    silentArgs     = "-unattend -force -full -log `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
