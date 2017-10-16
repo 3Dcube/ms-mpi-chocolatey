4dcd3fcc4f6add476918852f2b54543baa0d623d
Vladislav Nemichev <vlpro@me.com>
2017-10-16 10:41:48 +0500

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = 'MS MPI'
+  fileType      = 'exe'
+  file         = Join-Path $toolsDir 'MSMpiSetup.exe'
+
+  softwareName  = 'Microsoft MPI (8.1.12438.1084)'
+
+  silentArgs    = "-unattend -force -full -log `"$($env:TEMP)\$($packageName).MSMpi.MsiInstall.log`""
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
